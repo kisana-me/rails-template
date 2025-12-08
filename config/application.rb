d8202b142ab6545cc64884330dd9063e81c0ceb2
kisana <kisana@amiverse.net>
2025-12-08 19:11:21 +0900

    ADD confs

--- config/application.rb
+++ config/application.rb
@@ -1,27 +1,23 @@
 require_relative "boot"
-
 require "rails/all"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("app/services")
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.middleware.delete Rack::Runtime
+    config.action_view.field_error_proc = proc { |html_tag, _instance| html_tag }
+    config.session_store :cookie_store,
+                         key: "_app",
+                         domain: :all,
+                         tld_length: 2,
+                         same_site: :lax,
+                         secure: Rails.env.production?,
+                         httponly: true
   end
 end
